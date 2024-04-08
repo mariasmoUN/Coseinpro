d32b38253cd48d4e45ba9f247b954e179574719b
mariasmot <mariasmo@unal.edu.co>
2024-04-08 14:53:03 -0500

    Botón flotante wpp,cambio de iconos,menu

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
+  const btn_Wapp = document.querySelector(".go-wpp-container");
+  btn_Wapp.style.display = "none";
 }
 
 function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
+  const btn_Wapp = document.querySelector(".go-wpp-container");
+  btn_Wapp.style.display = "flex";
 }

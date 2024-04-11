4a7f5e349d460e9153d34884dc765e0d629d6f14
mariasmot <mariasmo@unal.edu.co>
2024-04-11 11:37:31 -0500

    boton flotante ir arriba

--- js/script.js
+++ js/script.js
@@ -1,87 +1,96 @@
+const sidebar = document.querySelector(".sidebar");
+const arriendos = document.querySelector(".arriendos");
+const arriendosHover = document.querySelector(".hoverArriendos");
+const ventas = document.querySelector(".ventas");
+const ventasHover = document.querySelector(".hoverVentas");
+const avaluos = document.querySelector(".avaluos");
+const avaluosHover = document.querySelector(".hoverAvaluos");
+const hipotecas = document.querySelector(".hipotecas");
+const hipotecasHover = document.querySelector(".hoverHipotecas");
+const asesorias = document.querySelector(".asesorias");
+const asesoriasHover = document.querySelector(".hoverAsesorias");
+const btn_Wapp = document.querySelector(".go-wpp-container");
+const btnTop = document.querySelector(".go-top-container");
+
 /* Menu */
 
 function showSidebar() {
-  const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
-  const btn_Wapp = document.querySelector(".go-wpp-container");
   btn_Wapp.style.display = "none";
+  btnTop.style.display = "none";
 }
 
 function hideSidebar() {
-  const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
-  const btn_Wapp = document.querySelector(".go-wpp-container");
   btn_Wapp.style.display = "flex";
+  btnTop.style.display = "flex";
 }
 
 /* Hover de los servicios */
 
 function showArriendos() {
-  const arriendos = document.querySelector(".arriendos");
   arriendos.style.display = "none";
-  const arriendosHover = document.querySelector(".hoverArriendos");
   arriendosHover.style.display = "flex";
 }
 
 function hideArriendos() {
-  const arriendos = document.querySelector(".arriendos");
   arriendos.style.display = "flex";
-  const arriendosHover = document.querySelector(".hoverArriendos");
   arriendosHover.style.display = "none";
 }
 
 function showVentas() {
-  const ventas = document.querySelector(".ventas");
   ventas.style.display = "none";
-  const ventasHover = document.querySelector(".hoverVentas");
   ventasHover.style.display = "flex";
 }
 
 function hideVentas() {
-  const ventas = document.querySelector(".ventas");
   ventas.style.display = "flex";
-  const ventasHover = document.querySelector(".hoverVentas");
   ventasHover.style.display = "none";
 }
 
 function showAvaluos() {
-  const avaluos = document.querySelector(".avaluos");
   avaluos.style.display = "none";
-  const avaluosHover = document.querySelector(".hoverAvaluos");
   avaluosHover.style.display = "flex";
 }
 
 function hideAvaluos() {
-  const avaluos = document.querySelector(".avaluos");
   avaluos.style.display = "flex";
-  const avaluosHover = document.querySelector(".hoverAvaluos");
   avaluosHover.style.display = "none";
 }
 
 function showHipotecas() {
-  const hipotecas = document.querySelector(".hipotecas");
   hipotecas.style.display = "none";
-  const hipotecasHover = document.querySelector(".hoverHipotecas");
   hipotecasHover.style.display = "flex";
 }
 
 function hideHipotecas() {
-  const hipotecas = document.querySelector(".hipotecas");
   hipotecas.style.display = "flex";
-  const hipotecasHover = document.querySelector(".hoverHipotecas");
   hipotecasHover.style.display = "none";
 }
 
 function showAsesorias() {
-  const asesorias = document.querySelector(".asesorias");
   asesorias.style.display = "none";
-  const asesoriasHover = document.querySelector(".hoverAsesorias");
   asesoriasHover.style.display = "flex";
 }
 
 function hideAsesorias() {
-  const asesorias = document.querySelector(".asesorias");
   asesorias.style.display = "flex";
-  const asesoriasHover = document.querySelector(".hoverAsesorias");
   asesoriasHover.style.display = "none";
 }
+
+/* Botón flotante */
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 100) {
+    btnTop.classList.add("show");
+  } else {
+    btnTop.classList.remove("show");
+  }
+};
+
+btnTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});

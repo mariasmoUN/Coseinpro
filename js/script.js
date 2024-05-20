5c5ac3fbcb6d55065655762aab15151a083586ac
mariasmot <mariasmo@unal.edu.co>
2024-05-20 15:29:11 -0500

    Agregando feed de insta

--- js/script.js
+++ js/script.js
@@ -23,8 +23,63 @@ function showSidebar() {
 function hideSidebar() {
   sidebar.style.display = "none";
   btn_Wapp.style.display = "flex";
-  btnTop.style.display = "flex";
+  /*   btnTop.style.display = "flex"; */
 }
+
+/* Feed de instagram */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const accessToken =
+    "IGQWRNdFNJbGVsUF9kd1BjNlJmbDJVUFBtU1Y5TTR0R2g3dHQxS0ZASVlhxTHhDYVlxQk1jRnY0WjZA4OHAtVHRMNHRpYlZAzQUNRY1BkbEt3U2FKSEotUXE1bnVfTmE5ZAEIyREdiTWFsdlA2X0pibHVUQWFfSVVieTAZD";
+  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token=${accessToken}`;
+  let currentIndex = 0;
+  let photos = [];
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      photos = data.data;
+      const photosContainer = document.getElementById("photos");
+      photos.forEach((photo) => {
+        const photoElement = document.createElement("div");
+
+        photoElement.innerHTML = `
+                  <a href="${photo.permalink}" target="_blank">
+                      <img src="${photo.media_url}" alt="${photo.caption}" style="width:100%;"/>
+                  </a>
+              `;
+        photosContainer.appendChild(photoElement);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching Instagram photos: ", error);
+    });
+
+  /* Botones del carrusel */
+
+  const prevButton = document.getElementById("prev");
+  const nextButton = document.getElementById("next");
+  const carousel = document.getElementById("photos");
+
+  prevButton.addEventListener("click", function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateCarousel();
+    }
+  });
+
+  nextButton.addEventListener("click", function () {
+    if (currentIndex < photos.length - 1) {
+      currentIndex++;
+      updateCarousel();
+    }
+  });
+
+  function updateCarousel() {
+    const offset = currentIndex * -100;
+    carousel.style.transform = `translateX(${offset}%)`;
+  }
+});
 
 /* Hover de los servicios */
 
@@ -80,17 +135,15 @@ function hideAsesorias() {
 
 /* Botón flotante */
 
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
 window.onscroll = function () {
-  if (document.documentElement.scrollTop > 100) {
-    btnTop.classList.add("show");
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrolled > 100) {
+    document.getElementById("scrollToTopButton").style.display = "block";
   } else {
-    btnTop.classList.remove("show");
+    document.getElementById("scrollToTopButton").style.display = "none";
   }
 };
-
-btnTop.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
